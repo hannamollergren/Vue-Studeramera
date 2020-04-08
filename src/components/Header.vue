de5beeaabd3b0b15cedd44ada34839e500672582
Alexander Caspersson <alexander.casperssonrgmail.com>
2020-04-08 11:14:56 +0200

    globala klart

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h2 class="“logo”">Studera mera</h2>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.logo {
+  color: #f8a978;
+  padding: 1em;
+  margin: 0;
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hello">
+    <button>Fortsätt</button>
+
+    <input type="text" placeholder="Hej, vem är du?" />
+
+    <hr />
+  </div>
+</template>
