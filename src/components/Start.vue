4f7754f4f7686a52470d4b0e6af06184badc16d7
Hanna Möllergren <hannamollergren@gmail.com>
2020-04-16 08:13:20 +0200

    ändrade sökväg till bilder

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -3,13 +3,12 @@
 	<transition name="start" appear>
 		<h1>StuderaMera</h1>
 	</transition>
-    
 
-  <img src="https://lh3.googleusercontent.com/PhlaXsXsqmgEdHj3PoL87fp1ok_RhaAmZbxlZ6n5EGa10II4X-giNRziqkf9uiHQCShs1Bw3oVqGMmRnFdFNxULr4wvSBhW7WS9vcehCEyOUGdJjzhQBnhqg9biVAXQNxSLvzxy6CEsLbiRvbf2x6rQ2CW1CJVqVkIPHiA2OgQ7_eN9APpNvix-u-uS_3IC-3B1OT8OvuWvNZKmUyWqSais90_9GGxCgqcg2RYSrYYJ4rCsMtClIjrZsXMcuB62JWf9DCg7skWGqzHDfYWIigzwcyoZScLF8JU_2lK4U6SGk2-A1qsxFzbKT9qmxG7wzVpbW27iJ1uCtO3CRY9cJ1Rsm9RpgtjZkdY5iTyj4iV-qr1qgIPAxRhIiHdtXNW6k3Rxm3o2t9AFvdL8DOrt8cy4ODV9DO_euZfvr9hgH7JwxUdV9sIEDFcXv-QeeSYKi0LTYj66BF9W3nhR7u9zbf53M6N_zzVeQpQrrepXSswHxs4Ff1YrhTXrkeJhB1asPoe8IwtZzQH07kWkMe7_TB8GiKkpj-ZALtGoHcdrgoPIr5klESGW3EonI-fMzBR0h8Q--2hkCT59-1r2HvuPzcn5-tNA_ICkcZMUTkY6YT5a4HMO1-aqyJxpfYMqSvey_zq-88I3R-rNEOXWCB77ejzSLD5UiFCfF0KzD6otLHoun9Q-HTlY3G2H-Mof30w=w213-h206-no" alt="">
-  <input type="text" placeholder="Hej, vem är du?" v-model="name" key="name" />
-	
-  <img src="https://lh3.googleusercontent.com/cu2NBt703IMccunDC6anUc7d6Lt_OyxMYQFJwETdd75e4hzGHDClR5wVGcORiSvTbBYQ0mFODOr915BJ_DAMoGkCluzUgtN0ujuu8sWI_iPEKewZqSsprIxckxQh7p_KSNoqHW1BAzEMpmWVIXSPw7ifssrUl9VRGEyErK1NDaWlFPtrehkMt-8GFr_dNUl_bcG7BnpeUTYJmefwUP6u1Nnlfn09BNHuNGzuvKJfpOWjJe7-C9wiAnl6oKW3bX1u39tZp2NC9Zq7hXSc_tlPp89RmRUEfe-Dg4wSczUat6VHSdos7T7LEURPXKtb3pQPfWfOqezc37lObmlehNcsow_kmgt3NvKFbpVmkR7iKJsbS0h-5mmpJz6B-sXvj03k7O0zGluol3z6nJTm9d5rfhJ-T9ABRh7LViUcqeSQLDzkYbLa9IjRC5tjkzQiGPydoMgNMMw3jpJd98TvoDivwlKQWqCAHFeUZq_qqB_gziOv24siCPLwaZGxZ2lp_CPkMEF7OOCJXV15s0x89snzqgfXtOQyv7Iffw0uihfWorXmE-h3Yl0fCPsOjV2LDwqqbjxpFH7qEPEuO16gM9eOtpsv2_wan920r3FlO445dQpZvGAkJpDC07UpJZVDP-Nym7nm0s3I-4yxuKA1pHmOv_gQPg1k_ic6KHmu7RfL6I4sLgoLlSjJ_o5E09IIPw=s88-no" alt="button" height="60px" class='button-style' @click="saveAndNext">
-  <br><br>
+	<img src="https://lh3.googleusercontent.com/cUqo_qCicnepYFwKICwtfaXm0rlK3DYRSHR1tCImXVD56pKVp64wpNg1jvtg-HhRfIGHWsMjV4olQeW6f1hYLTCtGCWA3uB8pOxHuC3JO1UA5pdFzPzftm4aNa5YcWasjjqjWzNuWQ=w2400" alt="Student">
+
+	<input type="text" placeholder="Hej, vem är du?" v-model="name" key="name" />
+
+	<img src='https://lh3.googleusercontent.com/DmRIkCSZW8W6LCQ7BbihQdkAS_FB6ANnGfBPURvNa7UqTHGXhSfNe70yzIKdKXTvnr-wEGcf01WfAU6m3U3Jlzz1MRqMOCh_XXj-k20IGlrahcNaJciBShzZWTvP7I4tlL5ELrbJIw=s88-p-k' alt="button" height="60px" class='button-style' @click="saveAndNext">
   </div>
 </template>
 
@@ -69,7 +68,7 @@ input {
   opacity: 0;  
 }
 .start-enter-to {
-  transition: opacity 3s;
+  transition: opacity 2s;
 }
 
 </style>
